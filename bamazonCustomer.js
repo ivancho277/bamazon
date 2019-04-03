ec29b33a2555b04c219e14b4414275049ce437ea
ivancho277 <rouskoi@live.com>
2019-04-03 10:11:48 -0700

    a small chagne

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,95 +4,94 @@ var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "password",
     database: "bamazon_db"
-  });
+});
 
-  function displayAllData(){
-      connection.query("SELECT * FROM bamazon_db.products;", function(err, res) {
-          if(err) throw err;
-          console.table(res);
-          askCustomerID();
-      })
-  }
+function displayAllData() {
+    connection.query("SELECT * FROM bamazon_db.products;", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        askCustomerID();
+    })
+}
 
-  function updateDataBase(id, quantity){
-      var query = `UPDATE bamazon_db.products SET stock_quantity = stock_quantity - ${quantity}  WHERE item_id = ${id};`
-      connection.query(query, function(err, res) {
-          //console.table(res);
-          displayAllData();
-      })
-  }
-  function getItemByID(id){
-      connection.query("SELECT product_name FROM bamazon_db.products WHERE item_id = " + id, function(err, res) {
-          if(err) throw err;
-         // console.log(res)
-          console.log(`You chose ${res[0].product_name}`);
-          return res.product_name;
-      })
-  }
+function updateDataBase(id, quantity) {
+    var query = `UPDATE bamazon_db.products SET stock_quantity = stock_quantity - ${quantity}  WHERE item_id = ${id};`
+    connection.query(query, function (err, res) {
+        //console.table(res);
+        displayAllData();
+    })
+}
 
-  //displayAllData();
-  //askCustomerID();
-  function askCustomerID() {
-      inquirer.prompt([
-          {
-              type: "input",
-              name: "id",
-              message: "What is the ID of the poduct you would like to buy?",
-              validate: function(value) {
-                if (isNaN(value) === false) {
-                  return true;
+function getItemByID(id) {
+    connection.query("SELECT product_name FROM bamazon_db.products WHERE item_id = " + id, function (err, res) {
+        if (err) throw err;
+        // console.log(res)
+        console.log(`You chose ${res[0].product_name}`);
+        return res.product_name;
+    })
+}
+
+//displayAllData();
+//askCustomerID();
+function askCustomerID() {
+    inquirer.prompt([{
+                type: "input",
+                name: "id",
+                message: "What is the ID of the poduct you would like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
                 }
-                return false;
-              }
-          }
-        
-      ])
-      .then((res) => {
-          var id = res.id;
-          console.log(res.id)
-          getItemByID(id);
-        //   console.log(`You want to buy ${res.product_name}`);
-         askCustomerQuanitiy(id);
-        //displayAllData();
-      })
-  }
+            }
 
-  function askCustomerQuanitiy(id){
+        ])
+        .then((res) => {
+            var id = res.id;
+            console.log(res.id)
+            getItemByID(id);
+            //   console.log(`You want to buy ${res.product_name}`);
+            askCustomerQuanitiy(id);
+            //displayAllData();
+        })
+}
 
-      inquirer.prompt([
-          {
-          type: "input",
-          name: "quantity",
-          message: "How many would you like to buy?",
-          validate: function(value) {
+function askCustomerQuanitiy(id) {
+
+    inquirer.prompt([{
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to buy?",
+        validate: function (value) {
             if (isNaN(value) === false) {
-              return true;
+                return true;
             }
             return false;
-          }
+        }
 
-          }
-      ]).then((res) => {
-          //log how many you bought
-          console.log(`You bought ${res.quantity}`);
-          //update the db with new quanitity
-          updateDataBase(id, res.quantity)
-      })
-  }
+    }]).then((res) => {
+        //log how many you bought
+        console.log(`You bought ${res.quantity}`);
+        //update the db with new quanitity
+        updateDataBase(id, res.quantity)
+    })
+}
 
-  function runBamazonCustomer(){
+function runBamazonCustomer() {
     displayAllData();
     
 
-  }
-  runBamazonCustomer()
+
+}
+runBamazonCustomer()
